7c4b25d346fb5216a589554729353977c19bfa12
Arun Kannan Thilagar <athilaga@sundevils.asu.edu>
2023-04-06 00:42:49 -0700

    Add project files.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="fileUploadDiv">
+      <lable className="InputLable">Text Input:</lable>
+      <input type="text"></input>
+      <br></br>
+      <lable className="InputLable">File Input:</lable>
+      <input id="fileInput" type="file" ></input>
+      <br></br>
+      <button type="submit">Upload</button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
