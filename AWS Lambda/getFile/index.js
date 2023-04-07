bdc1f9b033d3a98efc2ce626ed2669c198af7101
Arun Kannan Thilagar <athilaga@sundevils.asu.edu>
2023-04-07 02:09:32 -0700

    fixing getFile lamba expression

--- AWS Lambda/getFile/index.js
+++ AWS Lambda/getFile/index.js
@@ -2,20 +2,33 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({region:"us-east-2"})
-exports.handler = function (event, context, callback) {
+
+exports.handler = async function (event, context, callback) {
     const ddb= new AWS.DynamoDB({apiVersion:"2012-10-08"});
-    const params={
-        TableName:"FileTable",
-        Key:{
-            id:{
-                s:"12345"
-            }
-        }
-    }
-    ddb.getItem(params,(err,data)=>{
-        if(err){
-            console.log(err);
-        }
-        console.log(data);
-    })
+    const documentClient = new AWS.DynamoDB.DocumentClient({region:"us-east-2"});
+    
+    
+const params = {
+  TableName: 'FileTable',
+  Key: {
+    id:"12345"
+  }
+};
+
+try{
+  const data = await documentClient.get(params).promise();
+  console.log(data);
+}
+catch(err){
+  console.log(err);
+}
+
+// Call DynamoDB to retrieve the item
+// ddb.getItem(params, (err, data) => {
+//   if (err) {
+//     console.log('Error', err);
+//   } else {
+//     console.log('Success', data.Item);
+//   }
+// });
 }
